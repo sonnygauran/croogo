e4e694b85cbc68ab695a96a98b6b7acfd7ec09fe
Julius Lagutan <jlagutan@meteomedia.com.ph>
2012-04-26 14:59:22 +0800

    Added routes parameter for station id.

--- plugins/weatherph/config/weatherph_routes.php
+++ plugins/weatherph/config/weatherph_routes.php
@@ -2,4 +2,5 @@
     CroogoRouter::connect('/', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'index'));
     CroogoRouter::connect('/getStations', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'getStations'));
     CroogoRouter::connect('/view', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'view'));
+    CroogoRouter::connect('/view/*', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'view'));
     
