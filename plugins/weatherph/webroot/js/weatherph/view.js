e58c32eb24fdb73ec11658510b7e48bff473b188
Julius Lagutan <jlagutan@meteomedia.com.ph>
2012-04-27 15:19:56 +0800

    Added the other tab script for detailed forecast and outlook markup.

--- plugins/weatherph/webroot/js/weatherph/view.js
+++ plugins/weatherph/webroot/js/weatherph/view.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    $('ul.tabs li').click(function(){
+    $('ul#week-forecast li').click(function(){
         
         $('ul#week-forecast li').each(function(index) {
             
@@ -15,6 +15,41 @@ $(document).ready(function(){
         
         $(this).addClass('current-tab');
         $('div#'+$(this).attr('id')).addClass('current-tab');
+        
+    });
+    
+    $('div#charts .tabs li').click(function(){
+        
+        $('div#charts .tabs li').each(function(index) {
+            
+            if($(this).hasClass('current-tab')){
+                
+                $(this).removeClass('current-tab').addClass('tab');
+                
+            }
+            
+        });
+        
+        $(this).addClass('current-tab');
+        
+        
+    });
+    
+    $('div#outlook .tabs li').click(function(){
+        
+        $('div#outlook .tabs li').each(function(index) {
+            
+            if($(this).hasClass('current-tab')){
+                
+                $(this).removeClass('current-tab').addClass('tab');
+                
+                
+            }
+            
+        });
+        
+        $(this).addClass('current-tab');
+        
         
     });
     
