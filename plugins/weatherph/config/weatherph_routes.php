41d1397a3413f1fcbacd884f035b2c6d9ec0f927
Julius Lagutan <jlagutan@meteomedia.com.ph>
2012-05-02 14:58:35 +0800

    Reroutes pages: Typhoon Preparedness, Typhoon Climatology, Typhoon Glossary, About, Impressum.

--- plugins/weatherph/config/weatherph_routes.php
+++ plugins/weatherph/config/weatherph_routes.php
@@ -3,4 +3,9 @@
     CroogoRouter::connect('/getStations', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'getStations'));
     CroogoRouter::connect('/view', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'view'));
     CroogoRouter::connect('/view/*', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'view'));
+    CroogoRouter::connect('/typhoonPreparedness', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'typhoonPreparedness'));
+    CroogoRouter::connect('/typhoonClimatology', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'typhoonClimatology'));
+    CroogoRouter::connect('/typhoonGlossary', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'typhoonGlossary'));
+    CroogoRouter::connect('/about', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'about'));
+    CroogoRouter::connect('/impressum', array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'impressum'));
     
