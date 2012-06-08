fc056e661e2502a5d13809c7020bd2830d0d8d5f
Jaggy Gauran <jaggygauran@gmail.com>
2012-06-08 15:17:18 +0800

    globalized properties for chart interfacing

--- plugins/meteomedia/libs/any_chart.php
+++ plugins/meteomedia/libs/any_chart.php
@@ -10,14 +10,22 @@ class AnyChart {
 
     private static $instance;
     
-    private $scale;
-    private $minorInterval;
-    private $showCrossLabel;
+    private $scale = 'XXXXXXXXXXXXXXXXXX';
+    private $minorInterval = 'XXXXXXXXXXXXXXXXXX';
+    private $showCrossLabel = 'XXXXXXXXXXXXXXXXXX';
     
-    private $titles = array(
-        'chart' => null,
-        'x_axis' => null,
-        'y_axis' => null,
+    private $properties = array(
+        'font' => array(
+            'family' => 'Helvetica',
+            'color' => '#444444',
+            'size' => '11',
+        ),
+        // Place null for empty titles
+        'titles' => array(
+            'chart' => null, 
+            'x_axis' => null,
+            'y_axis' => null,
+        )
     );
     
     
@@ -50,130 +58,7 @@ class AnyChart {
             )
         );
     
-    public $chart_settings = array(
-        'chart_settings' => array(
-            'properties' => array(),
     
-            'children' => array(
-                'title' => array(
-                    'properties' => array(
-                        'enabled' => 'false'
-                    ),
-                    'value' => null,
-                ),
-                'axes' => array(
-                    'properties' => array(),
-                    'children' => array(
-                        'x_axis' => array(
-                            'properties' => array(
-                                'enable' => 'true'
-                            ),
-                            'children' => array(
-                                'scale' => array(
-                                    'properties' => array(
-                                        'type'=>'DateTime',
-                                        'minimum_offset'=>'0',
-                                        'maximum_offset'=>'0',
-                                        'minor_interval'=>'XXXXXXXXXXXXXXXXXX',
-                                        'minor_interval_unit'=>'Hour',
-                                        'major_interval'=>'1',
-                                        'major_interval_unit'=>'Day',
-                                    ),
-                                    'value' => null
-                                ),
-                                'title' => array(
-                                    'properties' => array(
-                                        'enabled' => 'false'
-                                    ),
-                                    'value' => null,
-                                ),
-                                'labels' => array(
-                                    'properties' => array(
-                                        'enabled' => 'true',
-                                        'show_cross_label' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-                                        'allow_overlap'=>'true'
-                                    ),
-                                    'children' => array(
-                                        'format' => array(
-                                            'properties' => array(),
-                                            'value' => array(
-                                                '<![CDATA[ ]]>',
-                                                '{%Value}{dateTimeFormat:%ddd %dd.%MM.}'
-                                            ),
-                                        ),
-                                        'font' => array(
-                                            'properties' => array(
-                                                'family' => 'Arial',
-                                                'color' => '#444444',
-                                                'size' => '11',
-                                            ),
-                                            'value' => null,
-                                        ),
-
-                                    ),
-                                ),
-                                'major_grid' => array(
-                                    'properties' => array(
-                                        ''
-                                    ),
-                                    'children' => array(
-                                        'font' => array(
-                                            'properties' => array(),
-                                            'children' => array(
-                                                'interlaced_fills' => array(
-                                                    'properties' => array(),    
-                                                    'children' => array(
-                                                        'even' => array(
-                                                            'properties' => array(),
-                                                            'children' => array(
-                                                                'fill' => array(
-                                                                    'properties' => array(
-                                                                        'color' => 'rgb(245,245,245)',
-                                                                        'opacity' => '1'
-                                                                    ),
-                                                                    'value' => null
-                                                                )
-                                                            )
-                                                        )
-                                                    ),
-                                                )
-                                            ),
-                                        ),
-                                    ),
-                                ),
-                            ),
-                        ),
-                        'y_axis' => array(
-                            'properties' => array(),
-                            'children' => array(
-
-                            ),
-                        ),
-                        'extra' => array(
-                            'properties' => array(),
-                            'children' => array(
-
-                            ),
-                        ),
-                        'chart_background' => array(
-                            'properties' => array(),
-                            'children' => array(
-
-                            ),
-                        ),
-                        'data_plot_background' => array(
-                            'properties' => array(),
-                            'children' => array(
-
-                            ),
-                        ),
-                    ),
-
-                ),
-            )
-        )
-        
-    );
     
     public function __construct() {
         
@@ -185,7 +70,124 @@ class AnyChart {
         return self::$instance;
     }
 
-//    public function chartSettings(){
-//        
-//    }
+    public function chartSettings(){
+        $x_axis = array(
+                'properties' => array(
+                    'enable' => 'true'
+                ),
+                'children' => array(
+                    'scale' => array(
+                        'properties' => array(
+                            'type'=>'DateTime',
+                            'minimum_offset'=>'0',
+                            'maximum_offset'=>'0',
+                            'minor_interval'=>  $this->minorInterval,
+                            'minor_interval_unit'=>'Hour',
+                            'major_interval'=>'1',
+                            'major_interval_unit'=>'Day',
+                        ),
+                        'value' => null
+                    ),
+                    'title' => array(
+                        'properties' => array(
+                            'enabled' => (empty($this->properties['titles']['x_axis'])) ? 'false' : 'true'
+                        ),
+                        'value' => $this->properties['titles']['x_axis'],
+                    ),
+                    'labels' => array(
+                        'properties' => array(
+                            'enabled' => 'true',
+                            'show_cross_label' => $this->showCrossLabel,
+                            'allow_overlap'=>'true'
+                        ),
+                        'children' => array(
+                            'format' => array(
+                                'properties' => array(),
+                                'value' => array(
+                                    '<![CDATA[ ]]>',
+                                    '{%Value}{dateTimeFormat:%ddd %dd.%MM.}'
+                                ),
+                            ),
+                            'font' => array(
+                                'properties' => $this->properties['font'],
+                                'value' => null,
+                            ),
+
+                        ),
+                    ),
+                    'major_grid' => array(
+                        'properties' => array(
+                            'enabled' => 'true',
+                            'interlaced' => 'true',
+                        ),
+                        'children' => array(
+                            'interlaced_fills' => array(
+                                'properties' => array(),    
+                                'children' => array(
+                                    'even' => array(
+                                        'properties' => array(),
+                                        'children' => array(
+                                            'fill' => array(
+                                                'properties' => array(
+                                                    'color' => 'rgb(245,245,245)',
+                                                    'opacity' => '1'
+                                                ),
+                                                'value' => null
+                                            )
+                                        )
+                                    )
+                                ),
+                            )
+                        ),
+                    ),
+                ),
+        );
+        $chart_settings = array(
+            'chart_settings' => array(
+                'properties' => array(),
+
+                'children' => array(
+                    'title' => array(
+                        'properties' => array(
+                            'enabled' => (empty($this->properties['titles']['chart'])) ? 'false' : 'true'
+                        ),
+                        'value' => $this->properties['titles']['chart'],
+                    ),
+                    'axes' => array(
+                        'properties' => array(),
+                        'children' => array(
+                            'x_axis' => $x_axis,
+                            'y_axis' => array(
+                                'properties' => array(),
+                                'children' => array(
+
+                                ),
+                            ),
+                            'extra' => array(
+                                'properties' => array(),
+                                'children' => array(
+
+                                ),
+                            ),
+                            'chart_background' => array(
+                                'properties' => array(),
+                                'children' => array(
+
+                                ),
+                            ),
+                            'data_plot_background' => array(
+                                'properties' => array(),
+                                'children' => array(
+
+                                ),
+                            ),
+                        ),
+
+                    ),
+                )
+            )
+        );
+        
+        return $chart_settings;
+    }
 }
