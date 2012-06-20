d0fac6b5e6b3c4905a8ecc3e2bb174a20d54a6de
Sonny Gauran <sgauran@meteomedia.com.ph>
2012-06-20 09:22:36 +0800

    Added Station shell and model

--- vendors/shells/station.php
+++ vendors/shells/station.php
@@ -1,27 +1,33 @@
 <?php
 
 class StationShell extends Shell {
-    
+    public $tasks = array('Readings');
 
     function main() {
+        App::import('Model', 'Weatherph.WeatherphStation');
         App::import('Model', 'Weatherph.Station');
+        $WStation = new WeatherphStation();
         $Station = new Station();
-        $stations = array_filter($Station->find('all'));
+        $stations = $WStation->fetch();
         $counter = 0;
         
         foreach ($stations as $station) {
-            $query = "insert into stations  values (";
-            foreach($station as $value){
-                $query .= "'$value'";
-                $query .= ($counter == count($station)-1) ? '' : ',';
-                $counter++;
+            if (!empty($station)) {
+                $query = "insert into stations  values (NULL, ";
+
+                foreach($station as $value){
+                    $query .= "'$value'";
+                    $query .= ($counter == count($station)-1) ? '' : ',';
+                    $counter++;
+                }
+
+                $counter = 0;
+                $query .= ");";
+
+                $Station->query($query);
             }
-            $counter = 0;
-            $query .= ");";
-            $Station->query($query);
-
         }
+        
     }
-
 }
 
