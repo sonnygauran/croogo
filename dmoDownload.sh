323d073c2b4788b3e7f782f0d394ff2e7600116a
Martin de Lima <mdelima@meteomedia.com.ph>
2012-07-16 13:46:35 +0800

    updated DMO download script

--- dmoDownload.sh
+++ dmoDownload.sh
@@ -2,6 +2,10 @@
 
 DATE=`date +%Y%m%d`
 
-scp netuser@199.195.193.240:data/dmo/live/dmo$DATE.zip data/dmo/
+if [ ! -d data/dmo ]; then
+	echo "Creating directory at [`pwd`/data/dmo]"
+	mkdir -p data/dmo
+fi
+scp netuser@199.195.193.240:/data/dmo/live/dmo$DATE.zip data/dmo/
 unzip data/dmo/dmo$DATE.zip -d data/dmo/ *.csv
 rm data/dmo/dmo$DATE.zip
