7f963eb27053e67896771fea72837fd67a81c576
Julius Lagutan <jlagutan@meteomedia.com.ph>
2012-07-16 19:23:28 +0800

    Added updateAll.sh

---
+++ updateAll.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "`date`:: Script Started."
+./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_generate
+./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_import
+./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_readings
+./Vendor/cake/cake/console/cake -app `pwd` meteomedia readings_import
+./getMovies.sh
+echo "`date`:: Script Finished."
