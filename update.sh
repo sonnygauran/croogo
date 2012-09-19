4d432acd90b48f6ec765bee4a8dd2a08c9538df0
Luis Quisumbing <lquisumbing@meteomedia.com.ph>
2012-09-19 16:13:09 +0800

    Back up for update.sh of alpha server and updated script (update.sh) for updating local copy

---
+++ update.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+type=$1
+
+
+if [ "$type" = "" ]; then
+clear
+    echo "usage: ./update.sh [keyword]\n"
+    echo "~> Keywords\n"
+    echo "\tstations \tInserts new stations to your local database";
+    echo "\treadings  \tUpdate readings";
+    echo "\tdmo \t\tUpdates dmo";
+    echo "\tmovies \t\tGet the latest movies for your local copy";
+    echo "\tall \t\tUpdates all";
+    echo "\n"
+
+	read -p "Choose keyword [all]: " type
+fi
+
+if [ "$type" = "" ]; then
+	type="all"
+fi
+
+if [ "$type" = 'stations' ]; then
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_generate
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_import	
+
+
+elif [ "$type" = 'readings' ]; then
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_readings
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia readings_import
+
+
+elif [ "$type" = 'dmo' ]; then
+	echo "`date` :: Script Started."
+	./scripts/dmoDownload.sh
+	echo "`date` :: Script Finished."
+
+elif [ "$type" = 'movies' ]; then
+    echo "`date` :: Script Started."
+    ./scripts/getMovies.sh
+    echo "`date` :: Script Finished."
+
+elif [ "$type" = 'all' ]; then
+
+	echo "`date`:: Script Started."
+
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_generate
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_import
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia station_readings
+	./Vendor/cake/cake/console/cake -app `pwd` meteomedia readings_import
+	./scripts/getMovies.sh
+	./scripts/dmoDownload.sh
+
+	echo "`date`:: Script Finished."
+
+elif [ $type = "help" ]; then
+	
+	clear
+	echo "usage: ./update.sh [keyword]\n"
+	echo "~> Keywords\n"
+    echo "\tstations \tInserts new stations to your local database";
+    echo "\treadings  \tUpdate readings";
+    echo "\tdmo \t\tUpdates dmo";
+    echo "\tmovies \t\tGet the latest movies for your local copy";
+    echo "\tall \t\tUpdates all";
+
+	echo "\n"
+
+else
+clear
+    echo "usage: ./update.sh [keyword]\n"
+    echo "~> Keywords\n"
+    echo "\tstations \tInserts new stations to your local database";
+    echo "\treadings  \tUpdate readings";
+    echo "\tdmo \t\tUpdates dmo";
+    echo "\tmovies \t\tGet the latest movies for your local copy";
+    echo "\tall \t\tUpdates all";    
+	echo "\n"
+
+fi
+
+
