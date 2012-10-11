30736073041a94a45b931fd4cd8ebe2f4d2d48c9
Jaggy Gauran <jaggygauran@gmail.com>
2012-10-11 19:14:08 +0800

    uncommented out the filter

--- plugins/weatherph/weatherph_app_controller.php
+++ plugins/weatherph/weatherph_app_controller.php
@@ -2,52 +2,52 @@
 
 class WeatherphAppController extends AppController {
 
-//    public function beforeFilter() {
-//        parent::beforeFilter();
-//        $this->Node->Behaviors->attach('Containable');
-//        $show_alert = false;
+    public function beforeFilter() {
+        parent::beforeFilter();
+        $this->Node->Behaviors->attach('Containable');
+        $show_alert = false;
+
+        $allowed_referrers = array();
+
+
+        error_log("WEBROOT: " . $this->webroot);
+        switch($this->action){
+            case 'getDetailedForecast':
+                $allowed_referrers = array(
+                    $this->webroot . 'view/', // Linux Hosts
+                    $this->webroot . 'swf/AnyChart.swf' // OS X referrer
+                );
+
+                $this->log('Referer: ' . $this->referer());
+                if(!in_array($this->referer(), $allowed_referrers)){
+                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
+                    exit;
+                }else{
+                    $this->log('allowed');
+                }
+                break;
+                case 'getStations':
+                $allowed_referrers = array(
+                    $this->webroot, // Linux Hosts
+                );
 //
-//        $allowed_referrers = array();
-//
-//
-//        error_log("WEBROOT: " . $this->webroot);
-//        switch($this->action){
-//            case 'getDetailedForecast':
-//                $allowed_referrers = array(
-//                    $this->webroot . 'view/', // Linux Hosts
-//                    $this->webroot . 'swf/AnyChart.swf' // OS X referrer
-//                );
-//
-//                $this->log('Referer: ' . $this->referer());
-//                if(!in_array($this->referer(), $allowed_referrers)){
-//                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
-//                    exit;
-//                }else{
-//                    $this->log('allowed');
-//                }
-//                break;
-//                case 'getStations':
-//                $allowed_referrers = array(
-//                    $this->webroot, // Linux Hosts
-//                );
-////
-//                if(!in_array($this->referer(), $allowed_referrers)){
-//                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
-////                    $this->redirect(array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'index'));
-//                    $this->layout = 'default';
-//                }
-//                break;
-//            case 'getResultCoordinates':
-//                error_log("REFERERERER: " . $this->referer());
-//                if(!strstr($this->referer(), 'results')){
-//                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
-//                    exit;
-//                }
-//                break;
-//
-//
-//        }
-//
-//
-//    }
-}
+                if(!in_array($this->referer(), $allowed_referrers)){
+                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
+//                    $this->redirect(array('plugin' => 'weatherph', 'controller' => 'weatherph', 'action' => 'index'));
+                    $this->layout = 'default';
+                }
+                break;
+            case 'getResultCoordinates':
+                error_log("REFERERERER: " . $this->referer());
+                if(!strstr($this->referer(), 'results')){
+                    $this->log('Client: '. $_SERVER['REMOTE_ADDR'] . ' tried to access ' . $_SERVER[ 'REQUEST_URI' ] . '', 'FORBIDDEN');
+                    exit;
+                }
+                break;
+
+
+        }
+
+
+    }
+                }
