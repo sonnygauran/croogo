b498a3dc109e1902f6e46db4026fbb2972255631
Luis Quisumbing <lquisumbing@meteomedia.com.ph>
2012-10-19 19:19:42 +0800

    Updated deployment script

--- deploymentScripts/updateAlpha.sh
+++ deploymentScripts/updateAlpha.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+
 VERSION=$1
-
-
-		sudo rm -rf /var/www/html
-		sudo mkdir /var/www/html
-		sudo chown netuser:netuser /var/www/html
-        cp /data/code/alpha.$VERSION.zip .
-        unzip -d /var/www/html alpha.$VERSION.zip
-        cd /var/www/html
-        pwd
-        sudo sh -x ./update.sh alpha.$VERSION
-        sudo cp ~netuser/database.php html/config/database.php
+       
+	sudo rm -rf /var/www/html
+	sudo mkdir /var/www/html
+	sudo chown netuser:netuser /var/www/html
+	cp /data/code/alpha.$VERSION.zip .
+	unzip -d /var/www/html alpha.$VERSION.zip
+	cd /var/www
+	pwd
+	sudo sh -x ./update.sh alpha.$VERSION
+	sudo cp ~netuser/database.php html/config/database.php
         sudo cp ~netuser/settings.private.yml html/config/
-        sudo chown netuser:netuser html/config/database.php
-		sudo git clone git@github.com:sonnygauran/cakephp.git html/Vendor/cake
-~                                                            
+	sudo chown netuser:netuser html/config/database.php	
+	cp -r /data/code/cakephp.git html/Vendor/cake
+	sh -x /home/netuser/symlink.sh
+	cp /home/netuser/robots.txt /var/www/html/webroot/
