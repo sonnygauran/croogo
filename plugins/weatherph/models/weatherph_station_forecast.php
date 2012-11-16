074df7c6f8c5ca941db33041a860fb342c8305bd
Jaggy Gauran <jaggygauran@gmail.com>
2012-11-16 14:46:15 +0800

    fixed the 0.0 number format issue

--- plugins/weatherph/models/weatherph_station_forecast.php
+++ plugins/weatherph/models/weatherph_station_forecast.php
@@ -81,11 +81,11 @@ class WeatherphStationForecast extends WeatherphAppModel {
             
             if(key_exists('rr1h', $current_reading) && key_exists('rain6', $current_reading)){
                 if(trim($current_reading['rr1h']) != ''){
-                    $current_readings['precipitation'] = ($current_reading['rr1h'] < 0)? "0mm" : number_format($current_reading['rr1h'], 1) . "mm";    
+                    $current_readings['precipitation'] = ($current_reading['rr1h'] < 0.1)? "0mm" : number_format($current_reading['rr1h'], 1) . "mm";    
                     $current_readings['precipitation_hr_range'] = "(1H)";
                 }else{
                     if(trim($current_reading['rain6']) != ''){
-                        $current_readings['precipitation'] = ($current_reading['rain6'] < 0)? "0mm" : number_format($current_reading['rain6'], 1) . "mm";  
+                        $current_readings['precipitation'] = ($current_reading['rain6'] < 0.1)? "0mm" : number_format($current_reading['rain6'], 1) . "mm";  
                         $current_readings['precipitation_hr_range'] = "(6H)"; 
                     }
                 }
